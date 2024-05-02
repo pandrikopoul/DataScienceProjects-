a6b557a8cccbf32e953019b8ed2fa58354524140
1780743 <p.andrikopoulos@students.uu.nl>
2024-05-02 14:09:38 +0300

    /

--- prometheus/local-setup/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker compose up -d 
-
-./node_exporter-1.6.1.linux-amd64/node_exporter & 
--- rest_api/src/data_models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class TemperatureMeasurement(BaseModel):
-    timestamp: float
-    temperature: float
